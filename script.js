7e524fc6a20e16058f75095c02b4b4673d9e4de7
johnbobthescob <lennotau@gmail.com>
2025-09-06 23:45:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,30 @@ function calculateResult() {
     display.value = "Error";
   }
 }
+
+function backspace() {
+  display.value = display.value.slice(0, -1);
+}
+
+document.addEventListener("keydown", function(event) {
+  const key = event.key;
+  if (/[0-9]/.test(key)) {
+    appendValue(key);
+  } else if (key === ".") {
+    appendValue(".");
+  } else if (key === "+") {
+    appendValue("+");
+  } else if (key === "-") {
+    appendValue("-");
+  } else if (key === "*") {
+    appendValue("*");
+  } else if (key === "/") {
+    appendValue("/");
+  } else if (key === "Enter") {
+    calculateResult();
+  } else if (key === "Backspace") {
+    backspace();
+  } else if (key === "Escape") {
+    clearDisplay();
+  }
+});
